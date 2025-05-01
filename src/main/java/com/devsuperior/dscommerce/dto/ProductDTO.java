c217b3be6e838a085b30dfca7db367df29401389
msoares-br <cadmarcos@protonmail.com>
2025-05-01 12:08:15 -0300

    Melhorando a validação do campos dos e incluindo o campo imgUrl no OrderItemDTO.

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,14 +11,15 @@ public class ProductDTO {
 
     private Long id;
     
-    @Size(min = 3, max = 80, message = "Nome precisar ter de 3 a 80 caracteres")
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
     @NotBlank(message = "Campo requerido")
     private String name;
     
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
     private String description;
-    
+
+    @NotNull(message = "O preço deve ser preenchido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     
